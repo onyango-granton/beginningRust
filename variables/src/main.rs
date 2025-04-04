e6d4ade93d5ce81ee123b3634c2854f2719eb9af
gonyango <grantononyango@gmail.com>
2025-04-04 06:43:13 +0300

    gettingstrted with const values

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,7 @@ fn main() {
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    const LIMIT: u32 = 10000000;
+    println!("This is the limit {LIMIT}")
 }
