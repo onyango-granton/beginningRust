790cb5d0f5367b78f8d1e177e0da726dc61e4abe
gonyango <grantononyango@gmail.com>
2025-04-04 06:47:56 +0300

    shadowed values and scopes

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -5,5 +5,15 @@ fn main() {
     println!("The value of x is: {x}");
 
     const LIMIT: u32 = 10000000;
-    println!("This is the limit {LIMIT}")
+    println!("This is the limit {LIMIT}");
+
+    let shadowed_value = 7;
+    let shadowed_value = shadowed_value + 2;
+
+    {
+        let shadowed_value = shadowed_value * 2;
+        println!("This is the value of inner scope shadowedValue: {shadowed_value}");
+    }
+
+    println!("This is the value of main function scope shadowedValue : {shadowed_value}")
 }
