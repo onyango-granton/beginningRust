b30d6e7100783f8aa3735cd32c45c24a513ce328
gonyango <grantononyango@gmail.com>
2025-04-16 23:18:07 +0300

    user input

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+mod lib;
+
+use lib::*;
+use std::io;
+
+pub fn main() {
+    println!("Welcome to ze calculator...");
+
+    print!("Enter first number :");
+    let mut first_number : u16 = 0;
+    io::stdin()
+        .read_line(&mut first_number)
+        .expect("Invalid number");
+
+    print!("Enter Operation :");
+    let mut operation = String::new();
+    io::stdin()
+        .read_line(&mut operation)
+        .expect("Invalid operation")
+
+    print!("Enter second number :");
+    let mut second_number : u16 = 0;
+    io::stdin()
+        .read_line(&mut second_number); 
 }
