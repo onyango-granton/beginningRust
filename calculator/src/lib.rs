5735244a8df9d8def85081d6938f069665933917
gonyango <grantononyango@gmail.com>
2025-04-17 06:32:49 +0300

    inputs come in as string

--- calculator/src/lib.rs
+++ calculator/src/lib.rs
@@ -13,3 +13,13 @@ pub fn multiply(num1: u16, num2:u16) -> u16 {
 pub fn divide(num1: u16, num2:u16) -> f64 {
     (&num1 / &num2) as f64
 }
+
+pub fn get_result(num1: &str, num2: &str, operand: &str) -> String {
+    match operand{
+        "+" => add(num1.parse::<u16>().unwrap(), num2.parse::<u16>().unwrap()).to_string(),
+        "-" => subtract(num1.parse::<u16>().unwrap(), num2.parse::<u16>().unwrap()).to_string(),
+        "*" => multiply(num1.parse::<u16>().unwrap(), num2.parse::<u16>().unwrap()).to_string(),
+        "/" => divide(num1.parse::<u16>().unwrap(), num2.parse::<u16>().unwrap()).to_string(),
+        &_ => "Invalid Input".to_string()
+    }
+}
--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -9,17 +9,18 @@ pub fn main() {
     print!("Enter first number :");
     let mut first_number : u16 = 0;
     io::stdin()
-        .read_line(&mut first_number)
+        .read_line(&mut first_number.to_string())
         .expect("Invalid number");
 
     print!("Enter Operation :");
     let mut operation = String::new();
     io::stdin()
-        .read_line(&mut operation)
-        .expect("Invalid operation")
+        .read_line(&mut operation.to_string())
+        .expect("Invalid operation");
 
     print!("Enter second number :");
     let mut second_number : u16 = 0;
     io::stdin()
-        .read_line(&mut second_number); 
+        .read_line(&mut second_number.to_string())
+        .expect("Invalid number"); 
 }
