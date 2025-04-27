543630fcfa0261e22e8e1ec670fcee6f306540c6
gonyango <grantononyango@gmail.com>
2025-04-27 12:55:56 +0300

    Implement function to check for similar characters between two strings

--- contain_similar_chars/src/main.rs
+++ contain_similar_chars/src/main.rs
@@ -1,3 +1,21 @@
+use std::collections::HashMap;
+
+fn contains_similar_chars(s1: &str, s2: &str) -> bool {
+    let mut s1_hash :HashMap<char, i32>= HashMap::new();
+
+    for ch in s1.chars(){
+        *s1_hash.entry(ch).or_insert(0) += 1;
+    }
+
+    for ch in s2.chars() {
+        if !(s1_hash.contains_key(&ch)){
+            return false;
+        }
+    }
+    
+    true
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", contains_similar_chars("listen", "silent"));
 }
