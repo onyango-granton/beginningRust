d08b92da5088c575733e9fd2524dbdadb8794067
gonyango <grantononyango@gmail.com>
2025-04-27 13:29:08 +0300

    Implement function to check if a string contains all alphabets

--- contains_all_alphabets/src/main.rs
+++ contains_all_alphabets/src/main.rs
@@ -1,3 +1,19 @@
+use std::collections::HashMap;
+
+fn contains_all_alphabets(s : &str) -> bool {
+    let s_vector = &s.chars().collect::<Vec<char>>();
+    let mut s_hash : HashMap<char, i32> = HashMap::new();
+
+    for ch in s_vector {
+        if ch.is_ascii_alphabetic(){
+            *s_hash.entry(ch.to_ascii_lowercase()).or_insert(0) += 1;
+        }
+    }
+
+    s_hash.len() == 26
+}
+
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("The quick brown fox jumps over the lazy dog");
+    println!("{:?} {:?} contains each alphabetical letter", contains_all_alphabets(&s), &s);
 }
